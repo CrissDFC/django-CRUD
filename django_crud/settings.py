5bc4ca215a2027e7f9db394d9700f696458755ad
Cristian Fonseca <cridafoca1@gmail.com>
2025-03-13 15:58:25 -0500

    solucion de integracion

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+
+from click import clear
 from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,11 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-
-    #Applications
-    'apps.task',
 ]
+
+NPM_BIN_PATH = "/opt/homebrew/bin/npm"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
